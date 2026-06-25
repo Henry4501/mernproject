35c1b5f5662e853947822d087d57b79d8ef84234
henry4501 <henasuvarnan@gmail.com>
2026-06-25 11:47:08 +0530

    created getbooks api

--- backend/server/server.js
+++ backend/server/server.js
@@ -5,11 +5,16 @@ const books = [{ id: 1, title: "Book 1", author: "Author 1" },
 { id: 2, title: "Book 2", author: "Author 2" },
 { id: 3, title: "Book 3", author: "Author 3" },
 ]
+app.get('/getbooks', (req, res) => {
+    res.json(books);
+});
+app.get('/', (req, res) => {
+  res.send('Library API is running');
+});
 const port=5000;
 app.use(express.json());
-app.get('/getdata',(req,res)=>{
-    res.send('Library API');
-});
+
 app.listen(port, () => {
   console.log(`Server is running on port 5000`);
+  console.log('Get Books: http://localhost:' + port + '/getbooks');
 });
